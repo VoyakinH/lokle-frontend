45ee756576c3aa769dcbeb12e7e138103ae58586
Aleksei Voyakin <aleksei_voyakin@mail.ru>
2022-07-11 01:39:04 +0300

    Изменены реквесты.

--- src/components/Requests.js
+++ src/components/Requests.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const postRequestHandler = async (path, data) => {
     let result;
     await axios.post(
-        `http://185.225.34.197:3001${path}`,
+        `http://185.225.34.197${path}`,
         JSON.stringify(data),
         { withCredentials: true }
     )
@@ -20,7 +20,7 @@ export const postRequestHandler = async (path, data) => {
 export const getRequestHandler = async (path) => {
     let result;
     await axios.get(
-        `http://185.225.34.197:3001${path}`,
+        `http://185.225.34.197${path}`,
         { withCredentials: true }
     )
         .then(response => {
